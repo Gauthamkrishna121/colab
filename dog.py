59ad15d315f4c27dd07a937852793927c11980d9
archanacs2004 <archanachandran8921@gamil.com>
2026-02-27 14:57:52 +0530

    monkey commit

---
+++ dog.py
@@ -0,0 +1,24 @@
+# Simple ASCII art to display a dog
+
+def display_dog():
+    dog = r"""
+     /^ ^\
+    / 0 0 \
+    V\ Y /V
+     / - \
+    |    \
+    || (__V
+    """
+    print(dog)
+
+if __name__ == "__main__":
+    # Simple ASCII art to display a monkey
+    monkey = r"""
+     w  c( .. )o   (
+      \__ (- )    __)
+          /\     (
+         /(_)___)
+        w  / |
+         |  \
+    """
+    print(monkey)
