4c43b4f22b67d9c9b8882e0c7ebe05ac907b6564
archanacs2004 <archanachandran8921@gamil.com>
2026-02-27 15:04:02 +0530

    gautham donkey commit

---
+++ gautham.py
@@ -0,0 +1,15 @@
+# ASCII art to display a donkey
+
+donkey = r"""
+           __  _
+       .-:'  `; `-._
+      (_,           )
+    ,'o"(            )>
+   (__,-'            )
+      (             )
+       `-'._.--._.-'
+          || || 
+         _||_||_
+"""
+
+print(donkey)
