0c7887271346b54db0e3ae643a8def39befedb95
Gauthamkrishna121 <lalapubgyt@gmail.com>
2026-02-27 14:59:56 +0530

    Girl push

--- dog.py
+++ dog.py
@@ -1,24 +1,9 @@
-# Simple ASCII art to display a dog
-
-def display_dog():
-    dog = r"""
-     /^ ^\
-    / 0 0 \
-    V\ Y /V
-     / - \
-    |    \
-    || (__V
+def print_girl():
+    girl = """
+    👧
+    /|\\
+    / \\
     """
-    print(dog)
+    print(girl)
 
-if __name__ == "__main__":
-    # Simple ASCII art to display a monkey
-    monkey = r"""
-     w  c( .. )o   (
-      \__ (- )    __)
-          /\     (
-         /(_)___)
-        w  / |
-         |  \
-    """
-    print(monkey)
+print_girl()
